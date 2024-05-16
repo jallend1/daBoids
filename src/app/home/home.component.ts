00d5dcfb6aaf9607be72a05ab5b52286a63ba37f
Jason Allen <jallend1@gmail.com>
2024-05-15 18:34:20 -0700

    Adds Boid location interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoidLocationComponent } from '../boid-location/boid-location.component';
+import { BoidLocation } from '../boidlocation';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +11,15 @@ import { BoidLocationComponent } from '../boid-location/boid-location.component'
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
+  boidLocation: BoidLocation = {
+    id: 9999,
+    name: 'Test Boid',
+    city: 'Test boid location',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
